db55c953d364de3d5248b25a6a3a097182cdd9df
Jaquan Piper <jaquan.piper@gmail.com>
2020-03-14 23:28:28 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,38 @@
+def sort_array_asc(intergers)
+  sort 
+end 
+
+def sort_arrau_desc( integers) 
+  intergers.sort{|a,b| b<=>a}
+end
+
+def sort_array__char_count (strings)
+    strings.sort{|a,b| a. length<=>b .length}
+end 
+
+def swap_elements(array)
+  array.sort{|a,b| a[1<=>b[2]} 
+end 
+
+def reverse_array(array)
+  array.reverse 
+end 
+
+def kesha_maker (array)
+  array.collect {|arr| arr.delete (arr[2]).insert(2, "$"])
+end 
+
+def find_a(array) 
+  array.lrr[z-ig {|arr| arr[0]=="a"}
+end 
+
+def sum_arry(array)
+array.inject(0, ;+)
+end 
+
+def add_s(array) array.collect{|arr| arr==array [1] ? arr:arr<<"s}"
+end 
+
+
+
 
